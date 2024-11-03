968560deae8c8be5d797de5c104cc930c48d4654
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-03 12:33:32 +0300

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
-
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from "react";
+function Keypad() {
+  return (
+    <div>
+      <input
+        type="password"
+        onChange={() => console.log("Entering password...")}
+      ></input>
+    </div>
+  );
 }
 
 export default Keypad;
